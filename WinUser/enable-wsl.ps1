0eb04f760a25db00e0c5bb3ffeeb810042eb9de5
sattiti <sattiti@users.noreply.github.com>
2021-01-18 14:35:51 +0900

    update

--- WinUser/enable-wsl.ps1
+++ WinUser/enable-wsl.ps1
@@ -1,5 +1,10 @@
+# Enable subsystem
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
 # 1. Enable virtualMachine feature
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
+
+# dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 # 2. you may have to reboot
 Restart-Computer
